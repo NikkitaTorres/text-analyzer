4845db5b1cf6cb029046937fb624b9f6936909de
Nikkita Torres <nikkitatorres@yahoo.com>
2023-10-24 13:00:22 -0700

    create all files and folders for the text analyzer

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+// Business Logic
+
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function(element) {
+    if (!Number(element)){
+    wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+
+// Business Logic
+
+function numberOfOccurrencesInText(word, text) {
+  if (word.trim().length === 0) {
+    return 0;
+  }
+  const textArray = text.split(" ");
+  let wordCount = 0;
+  textArray.forEach(function(element) {
+  if (element.toLowerCase().includes(word.toLowerCase())) {
+    wordCount++
+  }
+});
+    return wordCount;
+  }
+
+  // UI Logic
+
+function handleFormSubmission() {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
+
+function boldPassage(word, text) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  p.append(text);
+  return p;
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
