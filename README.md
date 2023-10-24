b36a66b4339bffc376474269244e09622d5b39ae
NikkitaTorres <nikkitatorres@yahoo.com>
2023-10-24 16:58:42 -0700

    update the site to include a function for displaying most common words

--- README.md
+++ README.md
@@ -92,3 +92,17 @@ const word = "hello";
 const text = "yo";
 boldPassage(word, text);
 Expected Output: <p>yo</p>
+
+Test: "It should return a matching word in a strong tag."
+Code:
+const word = "hello";
+const text = "hello";
+boldPassage(word, text);
+Expected Output: <p><strong>hello</strong></p>
+
+Test: "It should wrap words that match in strong tags but not words that don't."
+Code:
+const word = "hello";
+const text = "hello there";
+boldPassage(word, text);
+Expected Output: <p><strong>hello</strong> there</p>
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
+// Utility Logic
+
+function isEmpty(testString) {
+  return (testString.trim().length === 0);
+}
+
 // Business Logic
 
 function wordCounter(text) {
-  if (text.trim().length === 0) {
+  if (isEmpty(text)) {
     return 0;
   }
   let wordCount = 0;
@@ -18,7 +24,7 @@ function wordCounter(text) {
 // Business Logic
 
 function numberOfOccurrencesInText(word, text) {
-  if (word.trim().length === 0) {
+  if (isEmpty(word)) {
     return 0;
   }
   const textArray = text.split(" ");
@@ -32,7 +38,27 @@ function numberOfOccurrencesInText(word, text) {
   }
 
   // UI Logic
-
+  function boldPassage(word, text) {
+    if (isEmpty(word) || isEmpty(text)) {
+      return null;
+    }
+    const p = document.createElement("p");
+    let textArray = text.split(" ");
+    textArray.forEach(function(element, index) {
+      if (word === element) {
+        const bold = document.createElement("strong");
+        bold.append(element);
+        p.append(bold);
+      } else {
+        p.append(element);
+      }
+      if (index !== (textArray.length - 1)) {
+        p.append(" ");
+      }
+    });
+    return p;
+  }
+  
 function handleFormSubmission() {
   event.preventDefault();
   const passage = document.getElementById("text-passage").value;
@@ -41,15 +67,13 @@ function handleFormSubmission() {
   const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
   document.getElementById("total-count").innerText = wordCount;
   document.getElementById("selected-count").innerText = occurrencesOfWord;
-}
 
-function boldPassage(word, text) {
-  if ((text.trim().length === 0) || (word.trim().length === 0)) {
-    return null;
+  let boldedPassage = boldPassage(word, passage);
+  if(boldedPassage) {
+    document.querySelector("div#bolded-passage").append(boldedPassage);
+  } else {
+    document.querySelector("div#bolded-passage").innerText = null;
   }
-  const p = document.createElement("p");
-  p.append(text);
-  return p;
 }
 
 window.addEventListener("load", function() {
